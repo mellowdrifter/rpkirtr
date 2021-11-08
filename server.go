350bbbe6eff21057666f875b89f7efe9d134857a
Darren O'Connor <mellow.drifter@gmail.com>
2021-11-08 00:32:59 +0000

    play with some channels

--- server.go
+++ server.go
@@ -132,10 +132,10 @@ func run() error {
 		urls: urls,
 	}
 
+	ch := make(chan bool)
+	go rpki.status(ch)
 	// keep ROAs updated.
-	go rpki.updateROAs()
-
-	go rpki.status()
+	go rpki.updateROAs(ch)
 
 	// I'm listening!
 	rpki.listen(port)
@@ -157,60 +157,62 @@ func (s *CacheServer) listen(port int64) {
 }
 
 // Log current ROA status
-func (s *CacheServer) status() {
+func (s *CacheServer) status(ch chan bool) {
 	for {
-		s.mutex.RLock()
-		// Count how many ROAs we have.
-		var v4, v6 int
-		for _, r := range s.roas {
-			if r.Prefix.IP().Is4() {
-				v4++
-			} else {
-				v6++
+		if <-ch {
+			log.Println("received true over the channel")
+			s.mutex.RLock()
+			// Count how many ROAs we have.
+			var v4, v6 int
+			for _, r := range s.roas {
+				if r.Prefix.IP().Is4() {
+					v4++
+				} else {
+					v6++
+				}
 			}
-		}
 
-		log.Println("*** Status ***")
-		log.Printf("I currently have %d clients connected\n", len(s.clients))
-		for i, v := range s.clients {
-			log.Printf("%d: %s\n", i+1, v.addr)
-		}
-		log.Printf("Current serial number is %d\n", s.serial)
-		log.Printf("Last diff is %t\n", s.diff.diff)
-		log.Printf("Current size of diff is %d\n", len(s.diff.addRoa)+len(s.diff.delRoa))
-		if len(s.diff.addRoa) > 0 {
-			log.Printf("ROAs to be added:")
-			for _, v := range s.diff.addRoa {
-				log.Printf("%s Mask %d ASN %d", v.Prefix.IPNet().String(), v.Prefix.Bits(), v.ASN)
+			log.Println("*** Status ***")
+			log.Printf("I currently have %d clients connected\n", len(s.clients))
+			for i, v := range s.clients {
+				log.Printf("%d: %s\n", i+1, v.addr)
 			}
-		}
-		if len(s.diff.delRoa) > 0 {
-			log.Printf("ROAs to be deleted:")
-			for _, v := range s.diff.delRoa {
-				log.Printf("%s Mask %d ASN %d", v.Prefix.IPNet().String(), v.Prefix.Bits(), v.ASN)
+			log.Printf("Current serial number is %d\n", s.serial)
+			log.Printf("Last diff is %t\n", s.diff.diff)
+			log.Printf("Current size of diff is %d\n", len(s.diff.addRoa)+len(s.diff.delRoa))
+			if len(s.diff.addRoa) > 0 {
+				log.Printf("ROAs to be added:")
+				for _, v := range s.diff.addRoa {
+					log.Printf("%s Mask %d ASN %d", v.Prefix.IPNet().String(), v.Prefix.Bits(), v.ASN)
+				}
 			}
-		}
-		log.Printf("There are %d ROAs\n", len(s.roas))
-		log.Printf("There are %d IPv4 ROAs and %d IPv6 ROAs\n", v4, v6)
-		if !s.updates.lastCheck.IsZero() {
-			log.Printf("Last check was %v\n", s.updates.lastCheck.Format("2006-01-02 15:04:05"))
-		}
-		if !s.updates.lastError.IsZero() {
-			log.Printf("Last error checking update was %v\n", s.updates.lastError.Format("2006-01-02 15:04:05"))
-		}
-		if !s.updates.lastUpdate.IsZero() {
-			log.Printf("Last ROA change was %v\n", s.updates.lastUpdate.Format("2006-01-02 15:04:05"))
-		}
+			if len(s.diff.delRoa) > 0 {
+				log.Printf("ROAs to be deleted:")
+				for _, v := range s.diff.delRoa {
+					log.Printf("%s Mask %d ASN %d", v.Prefix.IPNet().String(), v.Prefix.Bits(), v.ASN)
+				}
+			}
+			log.Printf("There are %d ROAs\n", len(s.roas))
+			log.Printf("There are %d IPv4 ROAs and %d IPv6 ROAs\n", v4, v6)
+			if !s.updates.lastCheck.IsZero() {
+				log.Printf("Last check was %v\n", s.updates.lastCheck.Format("2006-01-02 15:04:05"))
+			}
+			if !s.updates.lastError.IsZero() {
+				log.Printf("Last error checking update was %v\n", s.updates.lastError.Format("2006-01-02 15:04:05"))
+			}
+			if !s.updates.lastUpdate.IsZero() {
+				log.Printf("Last ROA change was %v\n", s.updates.lastUpdate.Format("2006-01-02 15:04:05"))
+			}
 
-		var m runtime.MemStats
-		runtime.ReadMemStats(&m)
-		log.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-		log.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-		log.Printf("\tSys = %v MiB", bToMb(m.Sys))
-		log.Printf("\tNumGC = %v\n", m.NumGC)
-		log.Println("*** eom ***")
-		s.mutex.RUnlock()
-		time.Sleep(refreshROA)
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			log.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+			log.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+			log.Printf("\tSys = %v MiB", bToMb(m.Sys))
+			log.Printf("\tNumGC = %v\n", m.NumGC)
+			log.Println("*** eom ***")
+			s.mutex.RUnlock()
+		}
 	}
 }
 
@@ -277,16 +279,19 @@ func (s *CacheServer) remove(c *client) {
 }
 
 // updateROAs will update the server struct with the current list of ROAs
-func (s *CacheServer) updateROAs() {
+func (s *CacheServer) updateROAs(ch chan bool) {
 	for {
 		time.Sleep(refreshROA)
 		s.mutex.Lock()
 		s.updates.lastCheck = time.Now()
+
 		roas, err := readROAs(s.urls)
 		if err != nil {
 			log.Printf("Unable to update ROAs, so keeping existing ROAs for now: %v\n", err)
 			s.updates.lastError = time.Now()
 			s.mutex.Unlock()
+			log.Println("will send true over the channel")
+			ch <- true
 			continue
 		}
 
@@ -302,6 +307,9 @@ func (s *CacheServer) updateROAs() {
 		log.Printf("roas updated, serial is now %d\n", s.serial)
 
 		s.mutex.Unlock()
+		log.Println("will send true over the channel")
+		ch <- true
+
 		// Notify all clients that the serial number has been updated.
 		for _, c := range s.clients {
 			log.Printf("sending a notify to %s\n", c.addr)
